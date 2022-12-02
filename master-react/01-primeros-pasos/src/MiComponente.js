544b17d0344264335f2ce3e2b79a67abcd584c11
Pierre Bertram <pierre.bertram@oneclick.es>
2022-12-02 10:52:11 +0100

    Uso de etiqueta vacia para poder usar varias etiquetas en un fragmento JSX

--- master-react/01-primeros-pasos/src/MiComponente.js
+++ master-react/01-primeros-pasos/src/MiComponente.js
@@ -4,7 +4,7 @@ import React from "react";
 // Funciones del componente
 const MiComponente = () => {
 
-    return <p>Mi primer componente</p>;
+    return <><p>Mi primer</p><p> componente</p></>;
 };
 
 // Export
