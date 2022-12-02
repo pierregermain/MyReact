796abb54031c3cc681d4c61525aab0541838c953
Pierre Bertram <pierre.bertram@oneclick.es>
2022-12-02 21:55:48 +0100

    Uso de If

--- master-react/01-primeros-pasos/src/SegundoComponente.js
+++ master-react/01-primeros-pasos/src/SegundoComponente.js
@@ -3,24 +3,27 @@ import React from 'react'
 export const SegundoComponente = () => {
 
     const libros = ["Harry Potter", "GOT", "RHCP"];
+    //const libros = [];
 
   return (
     <div className='segundo-componente'>
-        <h1>Listado Libros</h1>
-        <p>Uso de array</p>
-        <ul>
-            <li>{libros[0]}</li>
-            <li>{libros[1]}</li>
-            <li>{libros[2]}</li>
-        </ul>
-        <p>Uso de map</p>
-        <ul>
-            {
-                libros.map((libro, indice) => {
-                    return <li key={indice}>{libro}</li>;
-                })
-            }
-        </ul>
+        {libros.length >= 1 ? 
+            (<>
+            <h1>Listado Libros</h1>
+            <p>Uso de array</p><ul>
+            <ul>
+                <li>{libros[0]}</li>
+                <li>{libros[1]}</li>
+                <li>{libros[2]}</li>
+            </ul>
+            <p>Uso de map. Uso de if</p>
+                      {libros.map((libro, indice) => {
+                          return <li key={indice}>{libro}</li>;
+                      })}
+                  </ul>
+            </>)
+        : (<p>No hay libros</p>)
+        }
     </div>
   )
 }
