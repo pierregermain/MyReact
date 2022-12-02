e369376f76db15f67a6dd50107de3b9251a3f608
Pierre Bertram <pierre.bertram@oneclick.es>
2022-12-02 22:16:00 +0100

    Uso de variables en vez de props

--- master-react/01-primeros-pasos/src/TercerComponente.js
+++ master-react/01-primeros-pasos/src/TercerComponente.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
-export const TercerComponente = (props) => {
-    console.log(props);
+export const TercerComponente = ({nombre, libro, ficha}) => {
   return (
     <div>
         <h1>Comunicacion entre componentes</h1>
-        <p>{props.nombre}</p>
-        <p>{props.ficha.estado}</p>
+        <p>{nombre}</p>
+        <p>{ficha.estado}</p>
     </div>
   )
 }
