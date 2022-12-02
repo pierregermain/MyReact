aea32d57c36380262a532204cbda29bbe439e99b
Pierre Bertram <pierre.bertram@oneclick.es>
2022-12-02 10:48:07 +0100

    Add mi primer componente

--- master-react/01-primeros-pasos/src/App.js
+++ master-react/01-primeros-pasos/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import MiComponente from './MiComponente';
 
 function App() {
   return (
@@ -7,6 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1> Hello World!</h1>
+        {/* Carga de mi primer componente */}
+        <MiComponente />
       </header>
     </div>
   );
---
+++ master-react/01-primeros-pasos/src/MiComponente.js
@@ -0,0 +1,11 @@
+// Importar modules react y dependencias
+import React from "react";
+
+// Funciones del componente
+const MiComponente = () => {
+
+    return <p>Mi primer componente</p>;
+};
+
+// Export
+export default MiComponente;
