7bbb8dfddfcaa66de6cb97ce3180ed15886584c6
Pierre Bertram <pierre.bertram@oneclick.es>
2022-12-02 12:03:01 +0100

    Acceder a partes de un componente

--- master-react/01-primeros-pasos/src/MiComponente.js
+++ master-react/01-primeros-pasos/src/MiComponente.js
@@ -1,14 +1,29 @@
 // Importar modules react y dependencias
-import React, from "react";
+import React from "react";
 
 // Funciones del componente
 const MiComponente = () => {
+
+    const nombre = "Pierre";
+    const web = "github.com";
+    let usuario = {
+        nombre: nombre,
+        apellido: 'Groening',
+        web: web
+    }
+
+    console.log(usuario);
 
     return (
         <div className="mi-componente">
             <hr></hr>
             <p>Mi primer</p>
             <p>componente</p>
+            <ul>
+              <li>Variable simple: {nombre}</li>
+              <li>Objeto.campo: <strong>{JSON.stringify(usuario.apellido)}</strong></li>
+              <li>Objeto: <strong>{JSON.stringify(usuario)}</strong></li>
+            </ul>
         </div>
     );
 };
