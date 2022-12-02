909a2a7d7d5965f321a70971a46063a8233ca127
Pierre Bertram <pierre.bertram@oneclick.es>
2022-12-02 10:37:47 +0100

    Limpieza codigo

--- master-react/01-primeros-pasos/src/App.js
+++ master-react/01-primeros-pasos/src/App.js
@@ -7,14 +7,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1> Hello World!</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
