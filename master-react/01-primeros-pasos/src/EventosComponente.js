b4ca6783c5774b2b4e70886cd045896a3d8e8bd9
Pierre Bertram <pierre.bertram@oneclick.es>
2022-12-07 12:12:11 +0100

    Add double click event

--- master-react/01-primeros-pasos/src/EventosComponente.js
+++ master-react/01-primeros-pasos/src/EventosComponente.js
@@ -5,11 +5,22 @@ export const EventosComponente = () => {
         console.log(evento);
         console.log("Hola " + nombre);
     }
+    const hasDadoDobleClick = () => {
+        console.log("Hola doble click");
+    }
   return (
     <div>
         <h1>Eventos en React</h1>
+
         {/*Evento click*/}
+        <p>
         <button onClick={ evento => hasDadoClick(evento, "Pierre") }>Dame click!</button>
+        </p>
+
+        {/*Evento doble click*/}
+        <p>
+        <button onDoubleClick={ hasDadoDobleClick }>Dame doble click!</button>
+        </p>
     </div>
   )
 }
