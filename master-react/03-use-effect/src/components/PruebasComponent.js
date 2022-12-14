0cf540c67d593708241f729490f9ac8da23de064
Pierre Bertram <pierre.bertram@oneclick.es>
2022-12-13 19:02:43 -0600

    Add contador to component

--- master-react/03-use-effect/src/components/PruebasComponent.js
+++ master-react/03-use-effect/src/components/PruebasComponent.js
@@ -7,20 +7,22 @@ export const PruebasComponent = () => {
         setUsuario(e.target.value);
     };
 
+    const [contador, setContador] = useState(0);
+
     useEffect(() => {
       console.log('has cargado el componente');
     
     }, [])
     
     useEffect(() => {
-      console.log('has cambiado el usuario (o cargado el componente)');
-    
+      setContador(contador+1);
+      console.log('Contador: '+contador);
     }, [usuario])
     
   return (
     <>
         <h1>El Efecto - Hook useEffect</h1>
-        <strong>{usuario}</strong>
+        <strong className={ contador >= 10 ? 'label-red' : 'label'}>{usuario}</strong>
         <form>
             <input type="text" onChange={modUsuario} placeholder="Cambia nombre" />
         </form>
