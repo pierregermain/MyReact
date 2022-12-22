4cf9ed43e2293305bcd566ff6934eeb8cdc946ae
Pierre Bertram <pierre.bertram@oneclick.es>
2022-12-22 10:54:50 -0600

    Obtener datos del formulario

--- master-react/04-formularios/src/components/FormularioComponent.js
+++ master-react/04-formularios/src/components/FormularioComponent.js
@@ -1,17 +1,32 @@
 import React from 'react'
 
 export const FormularioComponent = () => {
+
+    const conseguirDatosFormulario = e => {
+        e.preventDefault();
+        let datos = e.target;
+        console.log(datos.nombre.value);
+
+        let usuario = {
+            nombre:datos.nombre.value,
+            apellido:datos.apellido.value,
+            genero:datos.genero.value,
+            bio:datos.bio.value,
+        }
+        console.log(usuario);
+    }
+
   return (
     <div>
         <h1>Formularios con react</h1>
-        <form>
-            <input type="text" placeholder='Nombre'/>
-            <input type="text" placeholder='Apellido'/>
-            <select>
+        <form onSubmit={conseguirDatosFormulario}>
+            <input type="text" name='nombre' placeholder='Nombre'/>
+            <input type="text" name='apellido' placeholder='Apellido'/>
+            <select name='genero'>
                 <option value="hombre">Hombre</option>
                 <option value="mujer">Mujer</option>
             </select>
-            <textarea placeholder='Biografia'></textarea>
+            <textarea name='bio' placeholder='Biografia'></textarea>
 
             <input type="submit" value="Enviar" />
 
