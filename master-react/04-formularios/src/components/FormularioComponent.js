0e3c339bade7892260edb4e430825c84c8dde244
Pierre Bertram <pierre.bertram@oneclick.es>
2022-12-22 11:09:09 -0600

    Mostrar datos de formulario por pantalla

--- master-react/04-formularios/src/components/FormularioComponent.js
+++ master-react/04-formularios/src/components/FormularioComponent.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useState } from 'react';
 
 export const FormularioComponent = () => {
+
+    const [usuario, setUsuario] = useState({});
 
     const conseguirDatosFormulario = e => {
         e.preventDefault();
@@ -14,11 +17,20 @@ export const FormularioComponent = () => {
             bio:datos.bio.value,
         }
         console.log(usuario);
+
+        setUsuario(usuario);
     }
 
   return (
     <div>
         <h1>Formularios con react</h1>
+        {usuario.bio && usuario.bio.length > 0 &&
+            (
+                <div className='info_usuario label'>
+                    {usuario.nombre} {usuario.apellido} es {usuario.genero} y su biografia es: {usuario.bio}
+                </div>
+            )
+        }
         <form onSubmit={conseguirDatosFormulario}>
             <input type="text" name='nombre' placeholder='Nombre'/>
             <input type="text" name='apellido' placeholder='Apellido'/>
