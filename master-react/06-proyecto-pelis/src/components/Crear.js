ee066d8978fa010d10f361c44892986d384a8f8d
Pierre Bertram <pierre.bertram@oneclick.es>
2022-12-29 09:05:52 -0600

    Get data from Form

--- master-react/06-proyecto-pelis/src/components/Crear.js
+++ master-react/06-proyecto-pelis/src/components/Crear.js
@@ -1,13 +1,39 @@
 import React from 'react'
 
 export const Crear = () => {
+
+    const title = 'Añadir pelicula';
+
+    const getDataForm = e => {
+        // Para que no se recargue la page
+        e.preventDefault();
+
+        let target = e.target;
+        let title = target.title.value;
+        let description = target.description.value;
+
+        console.log(target);
+        alert('Formulario enviado');
+    }
+
   return (
     <div className="add">
-        <h3 className="title">Añadir pelicula</h3>
-        <form>
-            <input type="text" id="title" placeholder="Titulo" />
-            <textarea id="description" placeholder="Descripción"></textarea>
-            <input type="submit" id="save" value="Guardar" />
+        <h3 className="title">{title}</h3>
+        <form onSubmit={getDataForm}>
+            <input type="text" 
+                id="title" 
+                name='title'
+                placeholder="Titulo" />
+
+            <textarea 
+                id="description" 
+                name='description'
+                placeholder="Descripción">
+            </textarea>
+
+            <input type="submit" 
+                id="save" 
+                value="Guardar" />
         </form>
     </div>
   )
