2a20bb96b11c917a3c90e42ecec97bb1631affbd
Pierre Bertram <pierre.bertram@oneclick.es>
2022-12-30 11:49:18 -0600

    Use local storage

--- master-react/06-proyecto-pelis/src/components/Crear.js
+++ master-react/06-proyecto-pelis/src/components/Crear.js
@@ -25,8 +25,34 @@ export const Crear = () => {
 
         setPeliState(peli);
 
-        console.log(peli);
-        alert('Formulario enviado');
+        // Guardar en el local storage
+        saveInStorage(peli);
+
+    }
+
+    const saveInStorage = peli => {
+        // Obtener pelis del local storage
+        let pelis = JSON.parse(localStorage.getItem('pelis'));
+
+        console.log(pelis);
+
+        // Check si pelis es array
+        if(Array.isArray(pelis)){
+            console.log('añadir');
+            // Añadir peli
+            pelis.push(peli);
+        }
+        else{
+            // Crear array nuevo
+            pelis = [peli];
+        }
+
+        // Guardar en local storage
+        localStorage.setItem('pelis', JSON.stringify([pelis]));
+        console.log(pelis);
+
+        // Optional: Devolver peli
+        return peli;
     }
 
   return (
