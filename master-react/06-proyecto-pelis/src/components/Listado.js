f695bad5733cd9273ab4013a434758109d8026ac
Pierre Bertram <pierre.bertram@oneclick.es>
2023-01-03 21:26:39 +0100

    Get list of movies

--- master-react/06-proyecto-pelis/src/components/Listado.js
+++ master-react/06-proyecto-pelis/src/components/Listado.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const Listado = () => {
+
+  const [listState, setListState] = useState([]);
+
+  useEffect(() => {
+
+    console.log('componente del listado de pelis')
+    getPelis();
+
+  },[]);
+
+  const getPelis = () =>{
+    let peliculas = JSON.parse(localStorage.getItem("pelis"));
+
+    setListState(peliculas);
+
+    console.log(peliculas);
+  };
+
   return (
     <>
-            <article className="peli-item">
-                <h3 className="title">Desarrollo web</h3>
-                <p className="description">victorroblesweb.es</p>
+    {listState != null ? 
+      listState.map(peli => {
+        return (
+              <article key={peli.id} className="peli-item">
+                  <h3 className="title">{peli.title}</h3>
+                  <p className="description">{peli.description}s</p>
 
-                <button className="edit">Editar</button>
-                <button className="delete">Borrar</button>
-            </article>
+                  <button className="edit">Editar</button>
+                  <button className="delete">Borrar</button>
+              </article>
+        );
+      })
+    : <h2>No hay pelis para mostrar</h2>
+    }
+
     </>
   )
 }
