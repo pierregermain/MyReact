e9a77e91f94ce6600269bf0c8efb7b5043d097ab
Pierre Bertram <pierre.bertram@oneclick.es>
2023-01-19 15:40:48 +0100

    Create routes

--- master-react/07-rutas/src/routers/RouterPrincipal.js
+++ master-react/07-rutas/src/routers/RouterPrincipal.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Routes, Route, Link, BrowserRouter } from 'react-router-dom';
+import { Articulos } from '../components/Articulos';
+import { Contacto } from '../components/Contacto';
+import { Inicio } from '../components/Inicio';
 
 export const RouterPrincipal = () => {
   return (
-    <div>RouterPrincipal</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/inicio" element={<Inicio />} />
+        <Route path="/articulos" element={<Articulos />} />
+        <Route path="/contacto" element={<Contacto />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
