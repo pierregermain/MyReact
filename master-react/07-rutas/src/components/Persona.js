50696ed27be73f6a1d0baf349f2494b2ab4d6a82
Pierre Bertram <pierre.bertram@oneclick.es>
2023-01-23 17:27:28 +0100

    Add redirect

--- master-react/07-rutas/src/components/Persona.js
+++ master-react/07-rutas/src/components/Persona.js
@@ -1,16 +1,34 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 
 export const Persona = () => {
 
   const {nombre, apellido} = useParams();
+  const navegar = useNavigate();
+
+  const enviar = (e) => {
+    e.preventDefault();
+    let nombre = e.target.nombre.value;
+    let apellido = e.target.apellido.value;
+
+    // Redirect
+    let url = `/persona/${nombre}/${apellido}`; 
+    navegar(url);
+
+  }
 
   return (
     <div>
       {!nombre && <h1> No hay nombre</h1>}
       {!apellido && <h1>No hay apellido</h1>}
       <h1>Página de Persona: {nombre} {apellido}</h1>
+
+      <form onSubmit={enviar}>
+        <input type="text" name="nombre" />
+        <input type="text" name="apellido" /> 
+        <input type="submit" name="enviar" value="enviar" />
+      </form>
     </div>
   )
 }
