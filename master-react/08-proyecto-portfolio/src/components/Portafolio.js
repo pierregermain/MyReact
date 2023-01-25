d638a27ca376da598c5d4409cc04782a06bd395e
Pierre Bertram <pierre.bertram@oneclick.es>
2023-01-25 22:56:06 +0100

    Add portfolio temp data and bring it to the front

--- master-react/08-proyecto-portfolio/src/components/Portafolio.js
+++ master-react/08-proyecto-portfolio/src/components/Portafolio.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import { trabajos } from '../data/trabajos'
 
 export const Portafolio = () => {
   return (
-    <div>Portafolio</div>
+    <div className='page'>
+      <h1 className='heading'>Portafolio</h1>
+
+      {
+        trabajos.map(trabajo => {
+          <article>
+            <h2>{trabajo.nombre}</h2>
+          </article>
+        })
+      }
+
+    </div>
   )
 }
---
+++ master-react/08-proyecto-portfolio/src/data/trabajos.js
@@ -0,0 +1,23 @@
+export const trabajos = [
+  {
+    'id': 'arcmedio',
+    'nombre': 'Arc Mediano',
+    'url': 'arcmedio.example.com',
+    'techs': 'HTML, CSS, JS, PHP, MySQL',
+    'categoy': 'Desarrollo Web'
+  },
+  {
+    'id': 'fb',
+    'nombre': 'Facebook',
+    'url': 'facebook.com',
+    'techs': 'React',
+    'categoy': 'Desarrollo Web'
+  },
+  {
+    'id': 'twitter',
+    'nombre': 'Twitter',
+    'url': 'twitter.com',
+    'techs': 'HTML, CSS',
+    'categoy': 'Desarrollo Web'
+  }
+];
