4a7923f4bed0f56348f6d8ab7d27dbfcfe233b61
Pierre Bertram <pierre.bertram@oneclick.es>
2023-02-01 22:12:08 +0100

    Use useRef in a form

--- master-react/09-use-ref/src/App.js
+++ master-react/09-use-ref/src/App.js
@@ -1,19 +1,11 @@
+import { Formulario } from "./components/Formulario";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-Header">
+      <Formulario />
+      </div>
     </div>
   );
 }
--- master-react/09-use-ref/src/components/Formulario.js
+++ master-react/09-use-ref/src/components/Formulario.js
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 export const Formulario = () => {
+
+  const nombre = useRef();
+  const apellidos = useRef();
+  const email = useRef();
+  const miCaja = useRef();
+
+  const mostrar = e => {
+    e.preventDefault();
+
+    console.log(nombre.current.value);
+    console.log(apellidos.current.value);
+    console.log(email.current.value);
+
+    console.log(miCaja);
+    miCaja.current.classList.add('fondo-verde');
+    miCaja.current.innerHTML = "Formulario enviado";
+  }
+
   return (
     <div>
+
+      <div className="miCaja" ref={miCaja}>
+        <p>Pruebas con useRef</p>
+      </div>
+
+      <form onSubmit={mostrar}>
+        <input type="text" placeholder='Nombre' ref={nombre}/><br/>
+        <input type="text" placeholder='Apellidos' ref={apellidos}/><br/>
+        <input type="email" placeholder='Correo Electrónico' ref={email} /><br/>
+
+        <input type="submit" value="Enviar" /><br/>
+      </form>
+
+      <button onClick={() => nombre.current.select()}>Empezar a rellenar el formulario</button>
+      
       
     </div>
   )
