3c3ba1db13f93c8c41b9e0f46cb62a48e6639932
Pierre Bertram <pierre.bertram@oneclick.es>
2023-02-02 14:57:16 +0100

    Add Ajax call to get empleados

--- master-react/10-hook-useMemo/src/components/Empleados.js
+++ master-react/10-hook-useMemo/src/components/Empleados.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export const Empleados = React.memo(() => {
+export const Empleados = () => {
+
+  const[empleados,setEmpleados] = useState();
+
+  useEffect(() => {
+    conseguirEmpleados();
+  },[]);
+
+  const conseguirEmpleados = async() => {
+    const url = "https://reqres.in/api/users?page=2";
+    const peticion = await fetch(url);
+    const {data: empleados} = await peticion.json();
+
+    setEmpleados(empleados);
+
+    console.log(empleados);
+    console.log('Se ha ejecutado la petición ajax');
+  }
+
   console.log('Hola mundo desde listado de empleados');
+
+
   return (
-    <div>Empleados</div>
+    <div>
+      <ul className='empleados'>
+        {
+         empleados?.map(empleado => {
+          return <li key={empleado.id}>{empleado.first_name} {empleado.last_name}</li>
+        })}    
+      </ul>
+    </div>
   )
-})
+}
