84d646bd2c482b479489ca22eff0696c11a6033a
Pierre Bertram <pierre.bertram@oneclick.es>
2023-02-02 13:56:54 +0100

    Access to variable with useRef vs. no useRef

--- master-react/09-use-ref/src/components/Ejemplo.js
+++ master-react/09-use-ref/src/components/Ejemplo.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useState } from 'react';
 
 export const Ejemplo = () => {
 
   const [numeroSaludos,setNumeroSaludos] = useState(0);
+  const saludosEnCola = useRef(numeroSaludos);
+
+  useEffect(() => {
+    saludosEnCola.current = numeroSaludos;
+
+    setTimeout(() => {
+      console.log("----");
+      console.log("**Saludos en cola (usando useRef, muestra valor actualizado):"+saludosEnCola.current);
+      console.log("Saludos en cola (sin usar useRef, muestra valor con timeout):"+numeroSaludos);
+    },2000);
+
+  },[numeroSaludos]);
 
   const enviarSaludo = e => {
     setNumeroSaludos(numeroSaludos + 1);
