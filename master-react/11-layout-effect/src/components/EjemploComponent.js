d82c54d528ae4ecc8f80ba04fa51f6551bc98730
Pierre Bertram <pierre.bertram@oneclick.es>
2023-02-03 12:38:55 +0100

    Move caja from useLayoutEffect

--- master-react/11-layout-effect/src/components/EjemploComponent.js
+++ master-react/11-layout-effect/src/components/EjemploComponent.js
@@ -1,29 +1,45 @@
-import React from 'react'
-import { useEffect } from 'react';
-import { useLayoutEffect } from 'react';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react'
 
 export const EjemploComponent = () => {
 
+  const [mostrar, setMostrar] = useState(false);
+  const caja = useRef();
+  const boton = useRef();
+
+  // Cuando puedes notar que la carga va a ser mas lenta y se ve algún parpadeo usar "useLayoutEffect" que se ejecuta antes de renderizar
   useLayoutEffect(() => {
-    console.log("useLayoutEffect");
-    let caja = document.querySelector("#caja");
-    caja.innerHTML = "Hola desde LayoutEffect";
-  }, []);
+    console.log("useEffect: Componente cargado");
+    if (caja.current == null) return
+
+    const { bottom } = boton.current.getBoundingClientRect();
+
+    caja.current.style.top = `${bottom + 45}px`;
+    caja.current.style.left = `${bottom + 45}px`;
+
+  }, [mostrar]);
 
   useEffect(() => {
-    console.log("useEffect");
-    let caja = document.querySelector("#caja");
-    caja.innerHTML = "Hola desde useEffect";
-  }, [])
-  
+    console.log("useEffect: Componente cargado");
+    if(caja.current == null) return
 
+    const { bottom } = boton.current.getBoundingClientRect();
+
+    caja.current.style.top = `${bottom + 45}px`;
+    caja.current.style.left = `${bottom + 45}px`;
+
+  }, [mostrar]);
 
   return (
     <div>
       <h1>Ejemplo useEffect y useLayoutEffect</h1>
-      <div id="caja">
 
-      </div>
+      <button ref={boton} onClick={() => setMostrar(prev => !prev)}>Mostrar Mensaje</button>
+
+      {mostrar && (
+        <div id="caja" ref={caja}>
+          Hola soy un mensaje {mostrar}
+        </div>
+      )}
 
     </div>
   )
