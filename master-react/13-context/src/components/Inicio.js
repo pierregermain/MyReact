6b03df7cf4cb5613542d89c72eccb6d0e6e550c3
Pierre Bertram <pierre.bertram@oneclick.es>
2023-02-03 23:14:51 +0100

    Use info from context

--- master-react/13-context/src/components/Inicio.js
+++ master-react/13-context/src/components/Inicio.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { PruebaContext } from '../context/PruebaContext'
 
 export const Inicio = () => {
+
+  const infoCompartida = useContext(PruebaContext);
+
+  console.log(infoCompartida);
+
   return (
     <div>Inicio</div>
   )
