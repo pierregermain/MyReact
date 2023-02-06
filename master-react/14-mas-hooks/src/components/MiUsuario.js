1d793323f3b6cd9924d2e9582c3a6de5885b1f09
Pierre Bertram <pierre.bertram@oneclick.es>
2023-02-06 20:38:32 +0100

    Create chargin message

--- master-react/14-mas-hooks/src/components/MiUsuario.js
+++ master-react/14-mas-hooks/src/components/MiUsuario.js
@@ -1,40 +1,61 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const MiUsuario = () => {
 
-  const [usuario,setUsuario] = useState({
-    datos: null
+  const [usuario, setUsuario] = useState({
+    datos: null,
+    cargando: true
   });
 
-  const getUsuario = async(url) => {
-    const peticion = await fetch(url);
-    const {data} = await peticion.json();
-
+  const getUsuario = async (url) => {
     setUsuario({
-      datos: data
-    })
+      ...usuario,
+      cargando: true
+    });
 
-    console.log(data);
+    setTimeout(async () => {
+      const peticion = await fetch(url);
+      const { data } = await peticion.json();
+
+      setUsuario({
+        datos: data,
+        cargando: false
+      })
+
+      //console.log(data);
+
+    }, 1000);
 
   }
 
   const getId = e => {
     let id = parseInt(e.target.value);
-    let url = 'https://reqres.in/api/users/'+id;
+    let url = 'https://reqres.in/api/users/' + id;
 
     getUsuario(url);
   }
 
-  
+  useEffect(() => {
+    getUsuario('https://reqres.in/api/users/2');
+  }, []);
+
+
 
   return (
     <div>
       <h2>Usuario</h2>
-      <p>Datos del usuario</p>
-      <ul>
-        <li>{usuario?.datos?.first_name}</li>
-        <li>{usuario?.datos?.last_name}</li>
-      </ul>
+      <div>
+        {usuario.cargando ? 'Cargando...' : (
+          <>
+            <p>Datos del usuario</p>
+            <ul>
+              <li>{usuario?.datos?.first_name}</li>
+              <li>{usuario?.datos?.last_name}</li>
+            </ul>
+          </>
+        )}
+      </div>
+
       <input type="number" name="id" onChange={getId} />
 
     </div>
