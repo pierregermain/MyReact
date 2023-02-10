9a7e4a9ad58e9299552a642c6cd609b439f4c621
Pierre Bertram <pierre.bertram@oneclick.es>
2023-02-10 12:58:51 +0100

    Update script

--- master-react/15-blog/arrancar-proyecto.sh
+++ master-react/15-blog/arrancar-proyecto.sh
@@ -11,8 +11,8 @@ project="cd ~/webapps/MyReact/master-react/15-blog && npm run dev"
 postm="/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=Postman --file-forwarding com.getpostman.Postman @@u %U @@"
 dbgui="mongodb-compass %U"
 
-gnome-terminal --tab -e "bash -c 'printf \"\033]0;MONGO\007\"; eval \"$mongo\"'" \
-               --tab -e "bash -c 'printf \"\033]0;NODE\007\"; eval \"$nodejs\"'" \
-               --tab -e "bash -c 'printf \"\033]0;PROJECT\007\"; eval \"$project\"'" \
-               --tab -e "bash -c 'printf \"\033]0;POSTMAN\007\"; eval \"$postm\"'" \
-               --tab -e "bash -c 'printf \"\033]0;DBGUI\007\"; eval \"$dbgui\"'" \
+gnome-terminal --tab --title "MONGO DB" -- bash -c "eval $mongo; exec bash -i"
+gnome-terminal --tab --title "NODE JS" -- bash -c "eval $nodejs; exec bash -i"
+gnome-terminal --tab --title "VITE" -- bash -c "eval $project; exec bash -i"
+gnome-terminal --tab --title "POSTMAN GUI" -- bash -c "eval $postm; exec bash -i"
+gnome-terminal --tab --title "COMPASS GUI" -- bash -c "eval $dbgui; exec bash -i"
