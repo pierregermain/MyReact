c8caed7ab3512d7e7b9dcf2e47d1a6621efe485c
Pierre Bertram <pierre.bertram@oneclick.es>
2023-02-13 16:54:59 +0100

    Hardcode articles

--- master-react/15-blog/src/components/pages/Articulos.jsx
+++ master-react/15-blog/src/components/pages/Articulos.jsx
@@ -1,6 +1,31 @@
 import React from 'react'
+import { useState, useEffect } from 'react';
 
 export const Articulos = () => {
+
+  const [articulos, setArticulos] = useState([]);
+
+  useEffect(() => {
+    let data = [
+      {
+        _id: 1,
+        title: "title 1",
+        content: "content 1"
+      },
+      {
+        _id: 2,
+        title: "title 2",
+        content: "content 2"
+      },
+      {
+        _id: 3,
+        title: "title 3",
+        content: "content 3"
+      }
+    ];
+    setArticulos(data);
+  }, [])
+
   return (
     <>Articulos
       <article className="articulo-item">
