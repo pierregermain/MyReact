d87474f7958c85dca61757109688dbcf6b3d41c8
Pierre Bertram <pierre.bertram@oneclick.es>
2023-02-20 11:05:47 +0100

    Add start script

---
+++ api-rest-red-social/arrancar-proyecto.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo 'Script para abrir varias pestañas en Gnome Terminal con el MERN Stack'
+echo 'M = MongoDB — document database'
+echo 'E = Express(.js) — Node.js web framework'
+echo 'R = React(.js) — a client-side JavaScript framework'
+echo 'N = Node(.js) — the premier JavaScript web server'
+
+mongo="sudo systemctl start mongod"
+nodejs="cd /home/pierre/webapps/MyReact/api-rest-red-social && npm run start"
+#vite="cd ~/webapps/MyReact/master-react/15-blog && npm run dev"
+postm="/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=Postman --file-forwarding com.getpostman.Postman @@u %U @@"
+dbgui="mongodb-compass %U"
+
+gnome-terminal --tab --title "MONGO DB" -- bash -c "eval $mongo; exec bash -i"
+gnome-terminal --tab --title "NODE JS" -- bash -c "eval $nodejs; exec bash -i"
+#gnome-terminal --tab --title "VITE" -- bash -c "eval $vite; exec bash -i"
+gnome-terminal --tab --title "POSTMAN GUI" -- bash -c "eval $postm; exec bash -i"
+gnome-terminal --tab --title "COMPASS GUI" -- bash -c "eval $dbgui; exec bash -i"
---
+++ api-rest-red-social/index.js
@@ -0,0 +1,11 @@
+// Conexión a la db
+
+// Crear servidor node
+
+// Configurar cors
+
+// Convertir los datos del body a objetos js
+
+// Cargar conf rutas
+
+// Poner servidor a escuchar petiones http
