387037f67b74ccc0da141d2cf7348f2f5f50d717
Pierre Bertram <pierre.bertram@oneclick.es>
2023-02-28 19:34:00 +0100

    Hash password

--- api-rest-red-social/controller/userController.js
+++ api-rest-red-social/controller/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
 
 // Acciones de prueba
 const pruebaUser = (req, res) => {
@@ -20,14 +21,12 @@ const register = (req, res) => {
       message: "Validación no correcta"
     });
   }
-  // Crear Objeto usuarios
-  let user_to_save = new User(params);
 
-  // Control duplicados
+  // Control duplicados con el operador OR
   User.find({$or: [
-    {email:user_to_save.email.toLowerCase()},
-    {nick:user_to_save.nick.toLowerCase()},
-  ]}).exec((error, users) => {
+    {email:params.email.toLowerCase()},
+    {nick:params.nick.toLowerCase()},
+  ]}).exec(async(error, users) => {
     if(error) return res.status(500).json({
       status: "error",
       message: "Error en la consulta de búsqueda de usuarios"
@@ -38,17 +37,26 @@ const register = (req, res) => {
         message: "El usuario ya existe"
       })
     }
+
+    // Cifrar contraseña
+    let pwd_hashed = await(bcrypt.hash(params.password, 10));
+    params.password = pwd_hashed;
+
+    // Crear Objeto usuarios
+    let user_to_save = new User(params);
+
+    // Guardar Usuario en base de datos
+    return res.status(200).json({
+      status: "success",
+      message: "Acción de registros de usuarios",
+      user_to_save
+    });
+
+
   })
 
-  // Cifrar contraseña
 
-  // Guardar Usuario en base de datos
 
-  return res.status(200).json({
-    status: "success",
-    message: "Acción de registros de usuarios",
-    user_to_save
-  });
 }
 
 // Exportar acciones
