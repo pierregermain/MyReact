bedb2d9901b4a0797050f2339f4a16fd8d312f3d
Pierre Bertram <pierre.bertram@oneclick.es>
2023-02-28 19:57:45 +0100

    Save user in db

--- api-rest-red-social/controller/userController.js
+++ api-rest-red-social/controller/userController.js
@@ -12,7 +12,7 @@ const pruebaUser = (req, res) => {
 const register = (req, res) => {
   // Recoger datos peticion
   let params = req.body;
-  console.log(params);
+  console.log("params", params);
 
   // Validacion
   if (!params.name || !params.email || !params.password || !params.nick) {
@@ -23,15 +23,19 @@ const register = (req, res) => {
   }
 
   // Control duplicados con el operador OR
-  User.find({$or: [
-    {email:params.email.toLowerCase()},
-    {nick:params.nick.toLowerCase()},
-  ]}).exec(async(error, users) => {
+  User.find({
+    $or: [
+      {email: params.email.toLowerCase() },
+      {nick: params.nick.toLowerCase() },
+    ]
+  }).exec(async(error, users) => {
+
     if(error) return res.status(500).json({
       status: "error",
       message: "Error en la consulta de búsqueda de usuarios"
     });
-    if(users && users.lenght >= 1){
+
+    if (users && users.length >= 1){
       return res.status(200).send({
         status: "success",
         message: "El usuario ya existe"
@@ -46,17 +50,27 @@ const register = (req, res) => {
     let user_to_save = new User(params);
 
     // Guardar Usuario en base de datos
-    return res.status(200).json({
-      status: "success",
-      message: "Acción de registros de usuarios",
-      user_to_save
-    });
-
-
+    user_to_save.save((error, userStored) => {
+      if(error){
+        return res.status(500).send({
+          status: "error",
+          message: "Error al comunicar petición de guardado de usuario en la DB"
+        })
+      }
+      if(!userStored){
+        return res.status(500).send({
+          status: "error",
+          message: "Error al guardar usuario en la DB"
+        })
+      }
+      else {
+        return res.status(200).send({
+          status: "success",
+          message: "El usuario ha sido guardado en la db"
+        })
+      }
+    })
   })
-
-
-
 }
 
 // Exportar acciones
