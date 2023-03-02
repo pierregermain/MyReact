252de54045c07dbe5c8b1af3cbb394a7f70fec46
Pierre Bertram <pierre.bertram@oneclick.es>
2023-03-02 15:35:40 +0100

    Use of the auth middleware

--- api-rest-red-social/routes/userRouter.js
+++ api-rest-red-social/routes/userRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("../controller/userController");
+const Auth = require("../middlewares/auth");
 
 // Definir routes
-router.get("/prueba-user", UserController.pruebaUser);
+router.get("/prueba-user", Auth.auth, UserController.pruebaUser);
 router.post("/register", UserController.register);
 router.post("/login", UserController.login);
 
