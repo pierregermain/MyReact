3db2e81f4b68585830dfa3b72126a2f5953afa38
Pierre Bertram <pierre.bertram@oneclick.es>
2023-03-08 11:10:35 +0100

    Update follow controller

--- api-rest-red-social/controller/followController.js
+++ api-rest-red-social/controller/followController.js
@@ -7,8 +7,22 @@ const pruebaFollow = (req, res) => {
     message: "Mensaje enviado desde followController"
   })
 }
+// Acción de guardar un follow (Accion seguir)
+const save = (req,res) => {
+  return res.status(200).send({
+    status: "success",
+    message: "Método seguir"
+  })
+}
+
+// Acción de borrar un follow (Accion de dejar de seguir)
+
+// Acción listado de usuarios que estoy siguiendo
+
+// Acción listado de usuarios que me siguien
 
 // Exportar acciones
 module.exports = {
-  pruebaFollow
+  pruebaFollow,
+  save
 }
--- api-rest-red-social/routes/followRouter.js
+++ api-rest-red-social/routes/followRouter.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const FollowController = require("../controller/followController");
+//const middlewareCheck = require("../middlewares/auth");
 
 // Definir routes
 router.get("/prueba-follow", FollowController.pruebaFollow);
+//router.post("/save", middlewareCheck.auth, FollowController.save);
 
 // Export routes
 module.exports = router;
