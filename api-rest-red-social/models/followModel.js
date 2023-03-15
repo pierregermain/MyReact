a3c5429390e9885bd23e634c00e7027d308ea51d
Pierre Bertram <pierre.bertram@oneclick.es>
2023-03-15 13:01:38 +0100

    Add comments to followModel

--- api-rest-red-social/models/followModel.js
+++ api-rest-red-social/models/followModel.js
@@ -1,5 +1,6 @@
 const {Schema, model} = require("mongoose");
 
+// En la tabla Follow, el campo *user* es el ID del usuario que sigue al usuario *followed*.
 const FollowSchema = Schema({
   user: {
     type: Schema.ObjectId,
