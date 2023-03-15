3f5cce80b2a3e6cb8319bd50067c125444258586
Pierre Bertram <pierre.bertram@oneclick.es>
2023-03-15 14:50:49 +0100

    Fix the following controller action

--- api-rest-red-social/controller/followController.js
+++ api-rest-red-social/controller/followController.js
@@ -1,25 +1,33 @@
 const Follow = require("../models/followModel");
 const User = require("../models/userModel");
 
+// Importar dependencias
+const mongoosePaginate = require("mongoose-pagination");
+
 // Acciones de prueba
 const pruebaFollow = (req, res) => {
   return res.status(200).send({
     message: "Mensaje enviado desde followController"
   })
 }
-// Acción de guardar un follow (Accion seguir)
+// Acción de guardar un follow (Accion de seguir)
 const save = (req, res) => {
+
+  // Conseguir datos del body
   const params = req.body;
+  console.log(params);
 
-  // Sacar el id del usuario dentificado
+  // Sacar el id del usuario identificado
   const identity = req.user;
+  console.log('identity',identity);
 
-  // Crear objeto model
+  // Crear objeto con modelo follow
+  // followed es el usuario que voy a seguir
   let userToFollow = new Follow({
     user: identity.id,
     followed: params.followed
   });
-  console.log(params);
+
 
   // Guardar objeto en db
   userToFollow.save((error, followStored) => {
@@ -61,11 +69,11 @@ const unfollow = (req, res) => {
     "followed": followedId
   }).remove((error, followDeleted) => {
 
-    if(error || !followDeleted){
-    return res.status(500).send({
-      status: "error",
-      message: "Error al intentar dejar de seguir al usuario"
-    });
+    if (error || !followDeleted) {
+      return res.status(500).send({
+        status: "error",
+        message: "Error al intentar dejar de seguir al usuario"
+      });
 
     }
 
@@ -87,25 +95,33 @@ const unfollow = (req, res) => {
 const following = (req, res) => {
 
   // Id del usuario identificado
+  let userId = req.user.id
 
   // Ver si llega el ID por params
+  if (req.params.id) { userId = req.params.id; }
 
   // Comprobar la pagina que me llega por params
+  let page = 1;
+
+  if (req.params.page) { page = req.params.page; }
 
   // Ver cuantos elementos quiero por página
+  const itemsPerPage = 5;
 
   // Find a follow
+  Follow.find({ user: userId }).populate("user followed").exec((error, follows) => {
 
-  // Obtener datos de los usuarios
-
-  // Paginar con mongoose
-
-
+    // Obtener datos de los usuarios
+    // Paginar con mongoose
 
     return res.status(200).send({
       status: "success",
-      message: "Listado de usuarios que estoy siguiendo"
+      message: "Listado de usuarios que estoy siguiendo",
+      userId: userId,
+      follows
     });
+
+  });
 
 }
 
@@ -114,10 +130,10 @@ const following = (req, res) => {
 const followers = (req, res) => {
 
 
-    return res.status(200).send({
-      status: "success",
-      message: "Listado de usuarios que me siguen"
-    });
+  return res.status(200).send({
+    status: "success",
+    message: "Listado de usuarios que me siguen"
+  });
 
 }
 
