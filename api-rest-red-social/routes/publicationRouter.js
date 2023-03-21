28fe46e0982f9d2d6534a5cbd6f431e5fa0682bd
Pierre Bertram <pierre.bertram@oneclick.es>
2023-03-21 14:44:08 +0100

    Add page to route

--- api-rest-red-social/routes/publicationRouter.js
+++ api-rest-red-social/routes/publicationRouter.js
@@ -9,7 +9,7 @@ router.get("/prueba-publication", PublicationController.pruebaPublication);
 router.post("/save", check.auth, PublicationController.save);
 router.get("/detail/:id", check.auth, PublicationController.detail);
 router.delete("/remove/:id", check.auth, PublicationController.remove);
-router.get("/user/:id", check.auth, PublicationController.user);
+router.get("/user/:id/:page", check.auth, PublicationController.user);
 
 // Export routes
 module.exports = router;
